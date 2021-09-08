2563081840fd7655e999054baed4cb66350797a6
Slashbunny <demodevil@gmail.com>
2021-09-08 18:28:38 -0400

    Specify extra library path to pyinstaller (fixes #17)
    On Windows, pyinstaller was not seeing my globally installed site
    packages, which included the 'yaml' module. By explicitly listing the
    path, it corrects the issue with pyinstaller.
    
    This was never version specific, but likely due to how python was
    installed on my Windows environment.

--- build-win.bat
+++ build-win.bat
@@ -1,3 +1,3 @@
-pip install pyinstaller==3.6
-pyinstaller download.py -n gog-plugins-downloader --onefile
+pip install pyinstaller==4.5.1
+pyinstaller.exe download.py -n gog-plugins-downloader --onefile -p C:\Users\username\scoop\apps\python\current\Lib\site-packages\
 COPY gog-plugins.bat dist\
